787e4e27e7895b8edc2f4dc131665235717c9875
himeshp <panchal.himesh89@gmail.com>
2024-12-19 21:07:52 +0530

    added react-app

---
+++ front-end/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <div className="App">
+            <h1>Welcome to React</h1>
+            <p>This is a basic React application</p>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
